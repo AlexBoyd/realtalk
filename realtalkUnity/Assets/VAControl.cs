ca1fed1b5d4f06faf270f2a3d99878acf457daa4
AlexBoyd <alexander.ray.boyd@gmail.com>
2014-10-20 18:12:31 -0700

    Initial VA-Chart Implementation and toggling options in the gitignore file

---
+++ realtalkUnity/Assets/VAControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VAControl : MonoBehaviour {
+
+	public float VAChartScale = 25;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		transform.localPosition = new Vector2 (-horizontal * VAChartScale, vertical * VAChartScale);
+	}
+}
