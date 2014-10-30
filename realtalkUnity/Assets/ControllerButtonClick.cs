a3472a5b96dfe22b73e88c202628de949a5f2d0a
AlexBoyd <alexander.ray.boyd@gmail.com>
2014-10-29 21:16:39 -0700

    adding controller support

---
+++ realtalkUnity/Assets/ControllerButtonClick.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ControllerButtonClick : MonoBehaviour {
+
+    public KeyCode joystickButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log(FetchKey());
+        }
+        if (Input.GetKeyDown (joystickButton))
+        {
+            gameObject.GetComponent<UnityEngine.UI.Button>().onClick.Invoke();
+        }
+	}
+
+    KeyCode FetchKey()
+    {
+        KeyCode[] validKeyCodes=(KeyCode[])System.Enum.GetValues(typeof(KeyCode));
+
+        foreach(KeyCode i in validKeyCodes)
+        {
+            if (Input.GetKey (i))
+            {
+                return i;
+            }
+        }
+        
+        return KeyCode.None;
+    }
+     
+}
--- realtalkUnity/Assets/Fungus/Dialog/Commands/SayVideo.cs
+++ realtalkUnity/Assets/Fungus/Dialog/Commands/SayVideo.cs
@@ -13,26 +13,42 @@ namespace Fungus
         public MovieTexture movie;
         public new GUITexture guiTexture;
         public new AudioSource audio;
+        private bool skipped = false;
+        private bool playing = false;
 
         public override void OnEnter ()
         {
             Debug.Log (name);   
+            movie.Stop ();
+            skipped = false;
+            playing = true;
             guiTexture.texture = movie;
-            ((MovieTexture)guiTexture.texture).Stop();        
-            ((MovieTexture)guiTexture.texture).Play();
+            ((MovieTexture)guiTexture.texture).Play ();
 
             audio.clip = ((MovieTexture)guiTexture.texture).audioClip;
             audio.Play ();
             StartCoroutine (PlayMovieCoroutine ());
         }
-        
-        
 
-        private IEnumerator PlayMovieCoroutine()
+        private IEnumerator PlayMovieCoroutine ()
         {
-            yield return new WaitForSeconds (movie.duration-0.1f);
-            ((MovieTexture)guiTexture.texture).Pause();
-            Continue();
+            yield return new WaitForSeconds (movie.duration - 0.1f);
+            if (!skipped)
+            {
+                ((MovieTexture)guiTexture.texture).Pause ();
+                playing = false;
+                Continue ();
+            }
+        }
+        
+        private void Update ()
+        {
+            if (Input.GetKeyUp (KeyCode.Space) && playing)
+            {
+                skipped = true;
+                playing = false;
+                Continue ();
+            }
         }
     }
 }
