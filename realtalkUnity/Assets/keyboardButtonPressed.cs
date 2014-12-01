fd26e0e14e86a6d95e2fd7825614088a64d27dc5
Gandhar <Gandhar_Tannu@thecdm.ca>
2014-11-30 17:10:25 -0800

    - Toggle Mode works with keyboard

---
+++ realtalkUnity/Assets/keyboardButtonPressed.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class keyboardButtonPressed : MonoBehaviour 
+{
+	public KeyCode joystickButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.anyKey)
+		{
+			Debug.Log(FetchKey());
+		}
+		if (Input.GetKeyDown (joystickButton))
+		{
+			gameObject.GetComponent<UnityEngine.UI.Toggle>().isOn = true;
+		}
+	}
+	
+	KeyCode FetchKey()
+	{
+		KeyCode[] validKeyCodes=(KeyCode[])System.Enum.GetValues(typeof(KeyCode));
+		
+		foreach(KeyCode i in validKeyCodes)
+		{
+			if (Input.GetKey (i))
+			{
+				return i;
+			}
+		}
+		
+		return KeyCode.None;
+	}
+}
